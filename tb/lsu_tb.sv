d07c9b51064dc7a1380d449c56cda2340aee74e1
zGiahuyPham <giahuy.pham1009@gmail.com>
2025-01-09 22:23:46 +0700

    Update lsu_tb.sv

--- tb/lsu_tb.sv
+++ tb/lsu_tb.sv
@@ -9,10 +9,10 @@ module lsu_tb();
   
   logic [11:0] addr;
   logic [2:0]  rwsel;
-  logic [31:0] wdata;
+  logic [31:0] sdata;
   logic        wren;
   logic [31:0] sw, btn;
-  logic [31:0] rdata;
+  logic [31:0] ldata;
   logic [31:0] lcd, ledg, ledr;
   logic [31:0] hex0, hex1, hex2, hex3, hex4, hex5, hex6, hex7;
 
@@ -20,12 +20,12 @@ module lsu_tb();
     .clk(clk),
     .rst(1'b1),
     .addr(addr),
-    .wdata(wdata),
+    .sdata(sdata),
     .wren(wren),
     .sw(sw),
     .btn(btn),
     .rwsel(rwsel),
-    .rdata(rdata),
+    .ldata(ldata),
     .hex0(hex0),
     .hex1(hex1),
     .hex2(hex2),
@@ -41,9 +41,9 @@ module lsu_tb();
 
   int rand_addr, rand_data;
   
-  task write_random_data(input [11:0] t_addr);
+  task store_random_data(input [11:0] t_addr);
     rand_data = $random;
-    wdata = rand_data;
+    sdata = rand_data;
     addr = t_addr;
     wren = 1'b1;
     #10; // wait 1 clk
@@ -51,22 +51,22 @@ module lsu_tb();
     t_addr = 12'd0;
   endtask
  
-  task read_random_data(input [11:0] t_addr, input [31:0] data);
+  task load_random_data(input [11:0] t_addr, input [31:0] data);
     addr = t_addr;
     @(posedge clk);
     #1;
-    assert(rdata === data) else begin
-      $display("Wrong answer: at 0x%x read %x, expected %x", t_addr, rdata, data);
+    assert(ldata === data) else begin
+      $display("Wrong answer: at 0x%x load %x, expected %x", t_addr, ldata, data);
       $stop;
     end
     t_addr = 12'd0;
   endtask
 
-  task read_data(input [31:0] data, input [31:0] data1);
+  task load_data(input [31:0] data, input [31:0] data1);
     @(posedge clk);
     #1;
     assert(data === data1) else begin
-      $display("Wrong answer: read %x, expected %x", data1, data);
+      $display("Wrong answer: load %x, expected %x", data1, data);
       $stop;
     end
   endtask
@@ -85,8 +85,8 @@ module lsu_tb();
     $display("================ DMEM test ================");
     for(int i = 0; i < 2048; i++) begin
       rand_addr = $random() % 12'h800;
-      write_random_data(rand_addr);
-      read_random_data(rand_addr, rand_data);
+      store_random_data(rand_addr);
+      load_random_data(rand_addr, rand_data);
       #10;
     end
 
@@ -94,36 +94,36 @@ module lsu_tb();
     for(int i = 1; i <= 100; i++) begin
       $display("Running on test %0d", i);
       sw = $random;
-      read_random_data('h900, sw);
+      load_random_data('h900, sw);
       btn = $random;
-      read_random_data('h910, btn);
+      load_random_data('h910, btn);
     end
     
     $display("=============== OUTPUT test ===============");
     for(int i = 1; i <= 100; i++) begin
       $display("Running on test %0d", i);
-      write_random_data('h800);
-      read_data(rand_data, hex0);
-      write_random_data('h810);
-      read_data(rand_data, hex1);
-      write_random_data('h820);
-      read_data(rand_data, hex2);
-      write_random_data('h830);
-      read_data(rand_data, hex3);
-      write_random_data('h840);
-      read_data(rand_data, hex4);
-      write_random_data('h850);
-      read_data(rand_data, hex5);
-      write_random_data('h860);
-      read_data(rand_data, hex6);
-      write_random_data('h870);
-      read_data(rand_data, hex7);
-      write_random_data('h880);
-      read_data(rand_data, ledr);
-      write_random_data('h890);
-      read_data(rand_data, ledg);
-      write_random_data('h8A0);
-      read_data(rand_data, lcd);
+      store_random_data('h800);
+      load_data(rand_data, hex0);
+      store_random_data('h810);
+      load_data(rand_data, hex1);
+      store_random_data('h820);
+      load_data(rand_data, hex2);
+      store_random_data('h830);
+      load_data(rand_data, hex3);
+      store_random_data('h840);
+      load_data(rand_data, hex4);
+      store_random_data('h850);
+      load_data(rand_data, hex5);
+      store_random_data('h860);
+      load_data(rand_data, hex6);
+      store_random_data('h870);
+      load_data(rand_data, hex7);
+      store_random_data('h880);
+      load_data(rand_data, ledr);
+      store_random_data('h890);
+      load_data(rand_data, ledg);
+      store_random_data('h8A0);
+      load_data(rand_data, lcd);
     end
     
     $display("===================================== Accepted =====================================");
