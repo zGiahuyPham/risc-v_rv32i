03b9d077df3fdfc31e0c981c46a219b39e5df9bf
zGiahuyPham <giahuy.pham1009@gmail.com>
2025-01-09 22:30:48 +0700

    Update dmem.sv

--- src/dmem.sv
+++ src/dmem.sv
@@ -1,9 +1,9 @@
 module dmem #(parameter DEPTH = 2048) (
 	input  logic                     clk,
 	input  logic [$clog2(DEPTH)-1:0] addr,
-	input  logic [31:0]              wdata,
+	input  logic [31:0]              sdata,
 	input  logic                     wren,
-	output logic [31:0]              rdata
+	output logic [31:0]              ldata
 );
 
 	logic [3:0][7:0] mem[DEPTH/4];
@@ -12,15 +12,15 @@ module dmem #(parameter DEPTH = 2048) (
 
 	always @(posedge clk) begin
 		if(wren) begin
-			mem[maddr][0] <= wdata[7:0];
-			mem[maddr][1] <= wdata[15:8];
-			mem[maddr][2] <= wdata[23:16];
-			mem[maddr][3] <= wdata[31:24];
+			mem[maddr][0] <= sdata[7:0];
+			mem[maddr][1] <= sdata[15:8];
+			mem[maddr][2] <= sdata[23:16];
+			mem[maddr][3] <= sdata[31:24];
     end
 	end
 
 	always_comb begin
-		rdata = mem[maddr];
+		ldata = mem[maddr];
 	end
 
 endmodule
